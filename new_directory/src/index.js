a331aa89cdb395640246e8933437fd54559988af
mangeshd142 <mangeshd142@gmail.com>
2024-08-01 11:42:56 +0530

    Delete new_directory directory
    delete

--- new_directory/src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Controller from './js/Controller/Controller';
-ReactDOM.render(React.createElement(Controller), document.getElementById('root'));
--- new_directory/src/js/index.js
+++
@@ -1,5 +0,0 @@
-
-import store from "../js/store/index";
-import { addArticle } from "../js/actions/";
-window.store = store;
-window.addArticle = addArticle;
